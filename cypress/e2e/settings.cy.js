78e9e5d8322bf869556d10055a0fbf4011b5106f
s1rserg <sirserg05@gmail.com>
2025-08-30 15:05:26 +0300

    fix: refactor tests

--- cypress/e2e/settings.cy.js
+++ cypress/e2e/settings.cy.js
@@ -1,50 +1,84 @@
-import SignInPageObject from '../support/pages/signIn.pageObject';
-import SettingsPageObject from '../support/pages/settings.pageObject';
-import { faker } from '@faker-js/faker';
+/* eslint-disable semi */
+import SignInPageObject from '../support/pages/signIn.pageObject'
+import SettingsPageObject from '../support/pages/settings.pageObject'
+import { faker } from '@faker-js/faker'
 
-const signInPage = new SignInPageObject();
-const settingsPage = new SettingsPageObject();
-const newName = faker.internet.userName();
-const newBio = faker.lorem.text();
-const newEmail = faker.internet.email();
-const newPassword = faker.internet.password();
+const signInPage = new SignInPageObject()
+const settingsPage = new SettingsPageObject()
 
 describe('Settings page', () => {
-  let user;
+  let user
+  const newName = faker.internet.userName()
+  const newBio = faker.lorem.sentence()
+  const newEmail = faker.internet.email()
+  const newPassword = faker.internet.password()
 
   before(() => {
-    cy.task('db:clear');
-  });
+    cy.task('db:clear')
+  })
 
   beforeEach(() => {
     cy.task('generateUser').then((generateUser) => {
-      user = generateUser;
-      signInPage.visit();
-      cy.login(user.email, user.username, user.password);
+      user = generateUser
+      signInPage.visit()
+      cy.login(user.email, user.username, user.password)
 
-      signInPage.typeEmail(user.email);
-      signInPage.typePassword(user.password);
-      signInPage.clickSignInBtn();
-      settingsPage.visit();
-    });
-  });
+      signInPage.typeEmail(user.email)
+      signInPage.typePassword(user.password)
+      signInPage.clickSignInBtn()
 
-  it('should provide an ability to update info', () => {
-    settingsPage.userNameField.should('have.value', user.username);
-    settingsPage.userNameField.clear();
-    settingsPage.typeUserName(newName);
-    settingsPage.userNameField.should('have.value', newName);
-    settingsPage.bioField.should('have.value', '');
-    settingsPage.typeBio(newBio);
-    settingsPage.bioField.should('contain.text', newBio);
-    settingsPage.emailField.should('have.value', user.email);
-    settingsPage.emailField.clear();
+      settingsPage.visit()
+    })
+  })
 
-    settingsPage.typeEmail(newEmail);
+  it('updates username and persists after reload', () => {
+    settingsPage.userNameField.clear()
+    settingsPage.typeUserName(newName)
+    settingsPage.updateSettings()
 
-    settingsPage.emailField.should('have.value', newEmail);
-    settingsPage.typePassword(newPassword);
-    settingsPage.passwordField.should('have.value', newPassword);
-    settingsPage.updateSettings();
-  });
-});
+    cy.url().should('include', `/profile/${newName}`)
+
+    settingsPage.visit()
+    settingsPage.userNameField.should('have.value', newName)
+  })
+
+  it('updates bio and persists after reload', () => {
+    settingsPage.bioField.clear()
+    settingsPage.typeBio(newBio)
+    settingsPage.updateSettings()
+
+    cy.url().should('include', `/profile/${user.username}`)
+
+    settingsPage.visit()
+    settingsPage.bioField.should('have.value', newBio)
+  })
+
+  it('updates email and persists after reload', () => {
+    settingsPage.emailField.clear()
+    settingsPage.typeEmail(newEmail)
+    settingsPage.updateSettings()
+
+    cy.url().should('include', `/profile/${user.username}`)
+
+    settingsPage.visit()
+    settingsPage.emailField.should('have.value', newEmail)
+  })
+
+  it('updates password and allows re-login with the new password', () => {
+    settingsPage.typePassword(newPassword)
+    settingsPage.updateSettings()
+
+    cy.url().should('include', `/profile/${user.username}`)
+
+    // logout
+    cy.get('[data-cy="logout"]').click()
+
+    // login with new password
+    signInPage.visit()
+    signInPage.typeEmail(user.email)
+    signInPage.typePassword(newPassword)
+    signInPage.clickSignInBtn()
+
+    cy.get('[data-cy="user-menu"]').should('be.visible')
+  })
+})
